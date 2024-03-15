c9b8edecc713e8c15bf44ed4a9298ca5632b1f3e
Julien RICHARD <julien.richard@betc.com>
2024-03-15 17:17:03 +0100

    add repl

---
+++ command_help.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func callbackHelp() {
+	availableCommands := getCommands()
+	for _, cmd := range availableCommands {
+		fmt.Println(cmd.name, " - ", cmd.description)
+	}
+	fmt.Println()
+
+}
